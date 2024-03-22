1c9d5a1cf49ea4096565de246a7b140951fb3369
Manu343726 <Manu343726@gmail.com>
2024-03-22 23:42:19 +0100

    Development environment setup files

--- llvm/cmake/config-ix.cmake
+++ llvm/cmake/config-ix.cmake
@@ -457,7 +457,10 @@ endif()
 # By default, we target the host, but this can be overridden at CMake
 # invocation time.
 include(GetHostTriple)
-get_host_triple(LLVM_INFERRED_HOST_TRIPLE)
+#For some reason CMake is not able to infer the host triple inside the linux docker container,
+#so we leave this commented for now and manually set the triple manually with the LLVM_HOST_TRIPLE
+#parameter
+#get_host_triple(LLVM_INFERRED_HOST_TRIPLE)
 
 set(LLVM_HOST_TRIPLE "${LLVM_INFERRED_HOST_TRIPLE}" CACHE STRING
     "Host on which LLVM binaries will run")
